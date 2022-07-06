3b7506df9d633949b5201cc4fdafe8094645215e
imsagnik04 <sagniksamaddar5@gmail.com>
2022-07-06 14:28:19 +0530

    updated [06-07-2022]

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-app.use(express.json({limit: '1mb'}));
+app.use(express.json({limit: '1mb'})); 
 const Datastore = require('nedb');
 require('dotenv').config();
 
